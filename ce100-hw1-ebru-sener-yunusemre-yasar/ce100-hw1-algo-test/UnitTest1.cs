51e5608db1aedf3787512f9efd550923edd75211
yunussyasar <yunusyasar.tr2114@gmail.com>
2023-03-14 01:42:23 +0300

    Randomized Quick Sort unit test

--- ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-test/UnitTest1.cs
+++ ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-test/UnitTest1.cs
@@ -420,8 +420,417 @@ namespace ce100_hw1_algo_test
                 }
             }
         }
+        
 
+[TestClass]
+    public class QuickSortTests
+    {
+        [TestMethod]
+        public void Sort_SortsArrayOf10000RandomIntegers()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateRandomArray(10000);
+            int[] expectedArray = (int[])unsortedArray.Clone();
+            Array.Sort(expectedArray);
+
+            // Act
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateRandomArray(int size)
+        {
+            int[] array = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = RandomizedPartition(array, low, high);
+                QuickSort(array, low, pivotIndex);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int RandomizedPartition(int[] array, int low, int high)
+        {
+            int pivotIndex = new Random().Next(low, high);
+            int pivotValue = array[pivotIndex];
+            int i = low - 1;
+            int j = high + 1;
+            while (true)
+            {
+                do
+                {
+                    i++;
+                } while (array[i] < pivotValue);
+                do
+                {
+                    j--;
+                } while (array[j] > pivotValue);
+                if (i >= j)
+                {
+                    return j;
+                }
+                Swap(array, i, j);
+            }
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
     }
+
+
+[TestClass]
+    public class QuickSortTests2
+    {
+        [TestMethod]
+        public void Sort_SortsArrayOf10000DescendingIntegers()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateDescendingArray(10000);
+            int[] expectedArray = (int[])unsortedArray.Clone();
+            Array.Sort(expectedArray);
+
+            // Act
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateDescendingArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = size - i;
+            }
+            return array;
+        }
+
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = RandomizedPartition(array, low, high);
+                QuickSort(array, low, pivotIndex);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int RandomizedPartition(int[] array, int low, int high)
+        {
+            int pivotIndex = new Random().Next(low, high);
+            int pivotValue = array[pivotIndex];
+            int i = low - 1;
+            int j = high + 1;
+            while (true)
+            {
+                do
+                {
+                    i++;
+                } while (array[i] < pivotValue);
+                do
+                {
+                    j--;
+                } while (array[j] > pivotValue);
+                if (i >= j)
+                {
+                    return j;
+                }
+                Swap(array, i, j);
+            }
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
+[TestClass]
+    public class QuickSortTests3
+    {
+        [TestMethod]
+        public void Sort_SortsArrayOf10000EqualIntegers()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateEqualArray(10000);
+            int[] expectedArray = (int[])unsortedArray.Clone();
+
+            // Act
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateEqualArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = 1;
+            }
+            return array;
+        }
+
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = RandomizedPartition(array, low, high);
+                QuickSort(array, low, pivotIndex);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int RandomizedPartition(int[] array, int low, int high)
+        {
+            int pivotIndex = new Random().Next(low, high);
+            int pivotValue = array[pivotIndex];
+            int i = low - 1;
+            int j = high + 1;
+            while (true)
+            {
+                do
+                {
+                    i++;
+                } while (array[i] < pivotValue);
+                do
+                {
+                    j--;
+                } while (array[j] > pivotValue);
+                if (i >= j)
+                {
+                    return j;
+                }
+                Swap(array, i, j);
+            }
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
+[TestClass]
+    public class QuickSortTestsx
+    {
+        [TestMethod]
+        public void Sort_SortsArrayOf10000RandomIntegers()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateRandomArray(10000);
+            int[] expectedArray = (int[])unsortedArray.Clone();
+            Array.Sort(expectedArray);
+
+            // Act
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateRandomArray(int size)
+        {
+            int[] array = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = RandomizedPartition(array, low, high);
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int RandomizedPartition(int[] array, int low, int high)
+        {
+            int pivotIndex = new Random().Next(low, high);
+            int pivotValue = array[pivotIndex];
+            Swap(array, pivotIndex, high);
+            int i = low - 1;
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivotValue)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
+[TestClass]
+    public class QuickSortTestsw
+    {
+        [TestMethod]
+        public void Sort_SortsArrayOf10000IdenticalIntegers()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateIdenticalArray(10000, 42);
+            int[] expectedArray = (int[])unsortedArray.Clone();
+            Array.Sort(expectedArray);
+
+            // Act
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateIdenticalArray(int size, int value)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = value;
+            }
+            return array;
+        }
+
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = RandomizedPartition(array, low, high);
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int RandomizedPartition(int[] array, int low, int high)
+        {
+            int pivotIndex = new Random().Next(low, high);
+            int pivotValue = array[pivotIndex];
+            Swap(array, pivotIndex, high);
+            int i = low - 1;
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivotValue)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
+[TestClass]
+    public class QuickSortTestsz
+    {
+        [TestMethod]
+        public void Sort_SortsArrayOf10000ReverseSortedIntegers()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateReverseSortedArray(10000);
+            int[] expectedArray = (int[])unsortedArray.Clone();
+            Array.Sort(expectedArray);
+
+            // Act
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateReverseSortedArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = size - i;
+            }
+            return array;
+        }
+
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = RandomizedPartition(array, low, high);
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int RandomizedPartition(int[] array, int low, int high)
+        {
+            int pivotIndex = new Random().Next(low, high);
+            int pivotValue = array[pivotIndex];
+            Swap(array, pivotIndex, high);
+            int i = low - 1;
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivotValue)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+}
 }
 
 
