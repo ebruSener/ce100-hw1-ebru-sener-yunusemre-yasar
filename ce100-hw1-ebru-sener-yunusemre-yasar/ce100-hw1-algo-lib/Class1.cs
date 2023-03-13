99d9ee88d5e7c60a395915041fd31965dda39ef0
yunussyasar <yunusyasar.tr2114@gmail.com>
2023-03-13 23:53:23 +0300

    Recursive MergeSort

--- ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-lib/Class1.cs
+++ ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-lib/Class1.cs
@@ -33,6 +33,63 @@ namespace ce100_hw1_algo_lib
             return A;
         }
 
+        public static void RecursiveMergeSort(int[] array)
+        {
+            if (array == null || array.Length <= 1)
+            {
+                return;
+            }
+            int[] temp = new int[array.Length];
+            RecursiveSort(array, temp, 0, array.Length - 1);
+        }
+
+        private static void RecursiveSort(int[] array, int[] temp, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                RecursiveSort(array, temp, left, mid);
+                RecursiveSort(array, temp, mid + 1, right);
+                Merge(array, temp, left, mid, right);
+            }
+        }
+
+        private static void Merge(int[] array, int[] temp, int left, int mid, int right)
+        {
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+            while (i <= mid && j <= right)
+            {
+                if (array[i] <= array[j])
+                {
+                    temp[k] = array[i];
+                    i++;
+                }
+                else
+                {
+                    temp[k] = array[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i <= mid)
+            {
+                temp[k] = array[i];
+                i++;
+                k++;
+            }
+            while (j <= right)
+            {
+                temp[k] = array[j];
+                j++;
+                k++;
+            }
+            for (int l = left; l <= right; l++)
+            {
+                array[l] = temp[l];
+            }
+        }
 
 
     }
