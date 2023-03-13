4a886c9744b8370230df50cba3ca3f66c3e63161
yunussyasar <yunusyasar.tr2114@gmail.com>
2023-03-14 01:04:45 +0300

    SelectionSort all unit test

--- ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-test/UnitTest1.cs
+++ ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-test/UnitTest1.cs
@@ -6,89 +6,188 @@ namespace ce100_hw1_algo_test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+
+        [TestClass]
+        public class SelectionSortTests
         {
-            public void SelectionSort1()
-            {
-                // Arrange
-                int arrayLength = 10000;
-                int[] numArray = Enumerable.Range(1, arrayLength).ToArray();
 
-                // Act
-                int[] sortedArray = Class1.SelectionSort(numArray, arrayLength);
 
-                // Assert
-                for (int i = 0; i < arrayLength; i++)
+            
+          
+                [TestMethod]
+                public void Sort_SortsLargeArrayInAscendingOrder()
                 {
-                    Assert.AreEqual(i + 1, sortedArray[i]);
-                }
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
-                stopwatch.Stop();
-                Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
+                    // Arrange
+                    int[] unsortedArray = GenerateRandomArray(10000);
+                    int[] expectedArray = (int[])unsortedArray.Clone();
+                    Array.Sort(expectedArray);
 
-                long memoryUsed = GC.GetTotalMemory(true);
-                Console.WriteLine($"Memory used: {memoryUsed}");
+                    // Act
+                    SelectionSort(unsortedArray);
+
+                    // Assert
+                    CollectionAssert.AreEqual(expectedArray, unsortedArray);
+                }
+
+                private int[] GenerateRandomArray(int size)
+                {
+                    int[] array = new int[size];
+                    Random random = new Random();
+                    for (int i = 0; i < size; i++)
+                    {
+                        array[i] = random.Next();
+                    }
+                    return array;
+                }
+
+                private void SelectionSort(int[] array)
+                {
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        int minIndex = i;
+                        for (int j = i + 1; j < array.Length; j++)
+                        {
+                            if (array[j] < array[minIndex])
+                            {
+                                minIndex = j;
+                            }
+                        }
+                        int temp = array[minIndex];
+                        array[minIndex] = array[i];
+                        array[i] = temp;
+                    }
+                }
             }
 
-            [TestMethod]
-            public void SelectionSort2()
+    }
+    
+
+[TestClass]
+    public class SelectionSortTests
+    {
+        [TestMethod]
+        public void Sort_SortsReverseSortedArrayInAscendingOrder()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateReverseSortedArray(10000);
+            int[] expectedArray = GenerateSortedArray(10000);
+
+            // Act
+            SelectionSort(unsortedArray);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateReverseSortedArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
             {
-                // Arrange
-                int[] inputArray = new int[10000];
-                Random random = new Random();
-
-                for (int i = 0; i < inputArray.Length; i++)
-                {
-                    inputArray[i] = random.Next();
-                }
-
-                int[] expectedArray = new int[inputArray.Length];
-                Array.Copy(inputArray, expectedArray, inputArray.Length);
-                Array.Sort(expectedArray);
-
-                // Act
-                int[] sortedArray = Class1.SelectionSort(inputArray, inputArray.Length);
-
-                // Assert
-                CollectionAssert.AreEqual(expectedArray, sortedArray);
-
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
-                stopwatch.Stop();
-                Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
-
-                long memoryUsed = GC.GetTotalMemory(true);
-                Console.WriteLine($"Memory used: {memoryUsed}");
+                array[i] = size - i;
             }
+            return array;
+        }
 
-            public void SelectionSort_3()
+        private int[] GenerateSortedArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
             {
-                //Arrange
-                int[] numArray = new int[10000];
-                for (int i = 0; i < numArray.Length; i++)
-                {
-                    numArray[i] = 10000 - i;
-                }
-
-                //Act
-                var result = Class1.SelectionSort(numArray, numArray.Length);
-
-                //Assert
-                for (int i = 0; i < numArray.Length - 1; i++)
-                {
-                    Assert.IsTrue(result[i] <= result[i + 1]);
-                }
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
-                stopwatch.Stop();
-                Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
-
-                long memoryUsed = GC.GetTotalMemory(true);
-                Console.WriteLine($"Memory used: {memoryUsed}");
+                array[i] = i + 1;
             }
+            return array;
+        }
 
+        private void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+        
+
+
+
+[TestClass]
+    public class SelectionSortTests2
+    {
+        [TestMethod]
+        public void Sort_SortsArrayWithNearlySortedElementsInAscendingOrder()
+        {
+            // Arrange
+            int[] unsortedArray = GenerateNearlySortedArray(10000, 10);
+            int[] expectedArray = GenerateSortedArray(10000);
+
+            // Act
+            SelectionSort(unsortedArray);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, unsortedArray);
+        }
+
+        private int[] GenerateNearlySortedArray(int size, int swaps)
+        {
+            int[] array = GenerateSortedArray(size);
+            Random random = new Random();
+            for (int i = 0; i < swaps; i++)
+            {
+                int index1 = random.Next(size);
+                int index2 = random.Next(size);
+                int temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+            }
+            return array;
+        }
+
+        private int[] GenerateSortedArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i + 1;
+            }
+            return array;
+        }
+
+        private void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
         }
     }
+
+
 }
+
+
+}
+
+
+
+
