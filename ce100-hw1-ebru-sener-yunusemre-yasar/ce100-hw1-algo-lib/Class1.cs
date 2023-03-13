d8684470eeee8cc53a189a249d6d05b70db7fce3
Ebru Şener <ebru.sener.tr@gmail.com>
2023-03-13 23:47:40 +0300

    selection sort commited

---
+++ ce100-hw1-ebru-sener-yunusemre-yasar/ce100-hw1-algo-lib/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ce100_hw1_algo_lib
+{
+    public class Class1
+    {
+        public static int[] SelectionSort(int[] A, int n)
+        {
+            int tmp;
+            int min;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                min = i;
+
+                for (int j = i; j < n; j++)
+                {
+                    if (A[j] < A[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                tmp = A[i];
+                A[i] = A[min];
+                A[min] = tmp;
+            }
+
+            return A;
+        }
+
+
+
+    }
+}
